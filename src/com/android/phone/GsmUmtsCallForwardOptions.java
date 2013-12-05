f46df940394875255618fcfb44e4746b6459f524
Qiang Chen <cqiang@codeaurora.org>
2013-12-05 16:08:26 +0800

    Phone: Removing the unreachable call forward option
    Some carriers require to remove the  unreachable
    call forward option in GSM mode.
    
    PS1: AICP'fied. NO MSIM
    
    Change-Id: I18b782fe83256dcf57956b23e11cdf478a48979e

--- src/com/android/phone/GsmUmtsCallForwardOptions.java
+++ src/com/android/phone/GsmUmtsCallForwardOptions.java
@@ -10,11 +10,11 @@ import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceScreen;
 import android.provider.ContactsContract.CommonDataKinds.Phone;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.MenuItem;
 
 import java.util.ArrayList;
-
 
 public class GsmUmtsCallForwardOptions extends TimeConsumingPreferenceActivity {
     private static final String LOG_TAG = "GsmUmtsCallForwardOptions";
@@ -55,6 +55,13 @@ public class GsmUmtsCallForwardOptions extends TimeConsumingPreferenceActivity {
         mButtonCFNRy = (CallForwardEditPreference) prefSet.findPreference(BUTTON_CFNRY_KEY);
         mButtonCFNRc = (CallForwardEditPreference) prefSet.findPreference(BUTTON_CFNRC_KEY);
 
+        if (getResources().getBoolean(R.bool.join_call_forward)) {
+            int phoneType = getPhoneTypeBySubscription();
+            if (TelephonyManager.PHONE_TYPE_GSM == phoneType && mButtonCFNRc != null) {
+                prefSet.removePreference(mButtonCFNRc);
+            }
+        }
+
         mButtonCFU.setParentActivity(this, mButtonCFU.reason);
         mButtonCFB.setParentActivity(this, mButtonCFB.reason);
         mButtonCFNRy.setParentActivity(this, mButtonCFNRy.reason);
@@ -77,6 +84,11 @@ public class GsmUmtsCallForwardOptions extends TimeConsumingPreferenceActivity {
             // android.R.id.home will be triggered in onOptionsItemSelected()
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+    }
+
+    private int getPhoneTypeBySubscription() {
+        int phoneType = TelephonyManager.getDefault().getCurrentPhoneType();
+        return phoneType;
     }
 
     @Override
