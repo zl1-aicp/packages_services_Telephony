194141f5bfb5fb2cf78ed0f3794cb4598735535e
kaiyiz <kaiyiz@codeaurora.org>
2013-12-09 13:47:09 +0800

    TeleService: Prompt user the fixed dialing number can’t be empty
    It does not check if the fixed dialing number is null or not,
    so it also can be saved as FDN when the fixed dialing number is null.
    
    When the number is null, we do not save it as FDN, and show a toast
    to prompt user the fixed dialing number can’t be empty.
    
    CRs-Fixed: 522515
    
    Change-Id: I0a78be0657cdd35ed00058bb825d2e077f52b471

--- src/com/android/phone/EditFdnContactScreen.java
+++ src/com/android/phone/EditFdnContactScreen.java
@@ -274,8 +274,10 @@ public class EditFdnContactScreen extends Activity {
       *
       * TODO: Fix this logic.
       */
-     private boolean isValidNumber(String number) {
-         return (number.length() <= 20);
+     protected boolean isValidNumber(String number) {
+         // Although number is not empty(checked in method onClick), add null
+         // pointer check to ensure the robustness of the code
+         return (null != number) && (number.length() <= 20);
      }
 
 
@@ -413,6 +415,15 @@ public class EditFdnContactScreen extends Activity {
             } else if (v == mNumberField) {
                 mButton.requestFocus();
             } else if (v == mButton) {
+                // If number is empty, the fdn record is meaningless.
+                // But if name is empty and number isn't, name named number,
+                // it is can be saved.
+                if (TextUtils.isEmpty(mNumberField.getText())) {
+                    showStatus(getResources().getText(R.string.fdn_empty_number));
+                    return;
+                } else if (TextUtils.isEmpty(mNameField.getText())) {
+                    mNameField.setText(getNumberFromTextField());
+                }
                 // Authenticate the pin AFTER the contact information
                 // is entered, and if we're not busy.
                 if (!mDataBusy) {
