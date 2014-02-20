3adb6c06882eab9b69596dbbab89f0de6ec2be7f
David van Tonder <david.vantonder@gmail.com>
2014-02-20 20:55:20 +0100

    Move message blacklist to framework (2/3).
    Change-Id: Ie1bea95513b985e79da2e28c86b15a2042c98754

--- src/com/android/phone/PhoneGlobals.java
+++ src/com/android/phone/PhoneGlobals.java
@@ -261,6 +261,7 @@ public class PhoneGlobals extends ContextWrapper implements WiredHeadsetListener
     // For adding to Blacklist from call log 
     private static final String REMOVE_BLACKLIST = "com.android.phone.REMOVE_BLACKLIST";
     private static final String EXTRA_NUMBER = "number";
+    private static final String EXTRA_TYPE = "type";
     private static final String EXTRA_FROM_NOTIFICATION = "fromNotification";
 
 
@@ -724,10 +725,10 @@ public class PhoneGlobals extends ContextWrapper implements WiredHeadsetListener
     }
 
     /* package */ static PendingIntent getUnblockNumberFromNotificationPendingIntent(
-            Context context, String number) {
+            Context context, String number, int type) {
         Intent intent = new Intent(REMOVE_BLACKLIST);
         intent.putExtra(EXTRA_NUMBER, number);
-        intent.putExtra(EXTRA_FROM_NOTIFICATION, true);	
+        intent.putExtra(EXTRA_FROM_NOTIFICATION, true);
         return PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
     }
 
@@ -1115,10 +1116,11 @@ public class PhoneGlobals extends ContextWrapper implements WiredHeadsetListener
                 }
             } else if (action.equals(REMOVE_BLACKLIST)) {
                 if (intent.getBooleanExtra(EXTRA_FROM_NOTIFICATION, false)) {
-                    // Dismiss the notification that brought us here 
-                    notificationMgr.cancelBlacklistedCallNotification();
+                    // Dismiss the notification that brought us here
+                    int blacklistType = intent.getIntExtra(EXTRA_TYPE, 0);
+                    notificationMgr.cancelBlacklistedNotification(blacklistType);
                     BlacklistUtils.addOrUpdate(context, intent.getStringExtra(EXTRA_NUMBER),
-                            0, BlacklistUtils.BLOCK_CALLS);
+                            0, blacklistType);
                 }
             } else if (action.equals(ACTION_VIBRATE_60)) {
                 if (VDBG) Log.d(LOG_TAG, "mReceiver: ACTION_VIBRATE_60");
