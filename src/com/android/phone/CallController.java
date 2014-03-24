cd5283e110d30ca17e0d3a4217bed8d487b2b497
kaiyiz <kaiyiz@codeaurora.org>
2014-03-24 11:19:55 +0800

    Settings: Display error when MO a call in airplane mode without SIMcard
    When without SIM card inserted, service state cann't be
    "STATE_IN_SERVICE", so cause cann't dismiss the dialog.
    
    Add the judgement that has IccCard or not. First judge has IccCard,
    if has not, pop up "Mobile network not available" dialog. If has
    IccCard, then judge service state to work.
    
    CRs-Fixed: 633688
    
    PS1: AICP'fied by removing MSIM
    
    Change-Id: Iffc46f1125c535e664857888a742acd5fd7e411c

--- src/com/android/phone/CallController.java
+++ src/com/android/phone/CallController.java
@@ -32,6 +32,7 @@ import android.os.SystemProperties;
 import android.provider.CallLog.Calls;
 import android.telephony.PhoneNumberUtils;
 import android.telephony.ServiceState;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 
 /**
@@ -539,6 +540,14 @@ public class CallController extends Handler {
         }
     }
 
+    private boolean hasIccCard() {
+        TelephonyManager telephonyManager = TelephonyManager.getDefault();
+        if (telephonyManager.hasIccCard()){
+            return true;
+        }
+        return false;
+    }
+
     /**
      * Checks the current ServiceState to make sure it's OK
      * to try making an outgoing call to the specified number.
@@ -557,6 +566,9 @@ public class CallController extends Handler {
                 return CallStatusCode.SUCCESS;
 
             case ServiceState.STATE_POWER_OFF:
+                if (!hasIccCard()) {
+                    return CallStatusCode.OUT_OF_SERVICE;
+                }
                 // Radio is explictly powered off.
                 return CallStatusCode.POWER_OFF;
 
