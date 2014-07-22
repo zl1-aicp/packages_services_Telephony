63595a6635ed358930f8aad5d4e4c6bd7cbeaa0b
kecinzer <kecinzer@gmail.com>
2014-07-22 15:06:19 +0200

    (3/3) Contact details on incoming call
    Change-Id: I932714de440df471371f08f2b60be7d2bcbab2cf
    Signed-off-by: kecinzer <kecinzer@gmail.com>

--- src/com/android/phone/CallFeaturesSetting.java
+++ src/com/android/phone/CallFeaturesSetting.java
@@ -218,6 +218,8 @@ public class CallFeaturesSetting extends PreferenceActivity
             "button_choose_people_lookup_provider";
     private static final String BUTTON_CHOOSE_REVERSE_LOOKUP_PROVIDER =
             "button_choose_reverse_lookup_provider";
+    private static final String BUTTON_DETAILED_INCALL_INFO_KEY =
+            "button_detailed_incall_info";
 
     private Intent mContactListIntent;
 
@@ -320,6 +322,7 @@ public class CallFeaturesSetting extends PreferenceActivity
     private ListPreference mChooseForwardLookupProvider;
     private ListPreference mChoosePeopleLookupProvider;
     private ListPreference mChooseReverseLookupProvider;
+    private CheckBoxPreference mDetailedIncallInfo;
 
     private class VoiceMailProvider {
         public VoiceMailProvider(String name, Intent intent) {
@@ -592,6 +595,10 @@ public class CallFeaturesSetting extends PreferenceActivity
         } else if (preference == mCallEndSound){
             Settings.System.putInt(getContentResolver(), Settings.System.CALL_END_SOUND,
                     mCallEndSound.isChecked() ? 1 : 0);
+            return true;
+        } else if (preference == mDetailedIncallInfo){
+            Settings.AOKP.putInt(getContentResolver(), Settings.AOKP.DETAILED_INCALL_INFO,
+                    mDetailedIncallInfo.isChecked() ? 1 : 0);
             return true;
         }
         return false;
@@ -1772,6 +1779,10 @@ public class CallFeaturesSetting extends PreferenceActivity
         mChoosePeopleLookupProvider.setOnPreferenceChangeListener(this);
 
         restoreLookupProviders();
+
+        mDetailedIncallInfo = (CheckBoxPreference) findPreference(BUTTON_DETAILED_INCALL_INFO_KEY);
+        mDetailedIncallInfo.setChecked(Settings.AOKP.getInt(getContentResolver(),
+                Settings.AOKP.DETAILED_INCALL_INFO, 0) != 0 ? true : false);
 
         // create intent to bring up contact list
         mContactListIntent = new Intent(Intent.ACTION_GET_CONTENT);
