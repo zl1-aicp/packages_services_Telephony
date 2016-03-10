cae78652e35dc1306e165182427f32b6f2715afe
Adnan Begovic <adnan@cyngn.com>
2016-03-10 09:58:20 -0800

    telephony: Move COLP definition to android settings.
      Otherwise we pollute the bootclasspath with public cmsdk
      symbols.
    
      2) testBootClassPathIsClean(org.cyanogenmod.tests.versioning.unit.ClassPathTest)
    java.lang.AssertionError: Jar file /system/framework/telephony-common.jar should not have cyanogenmod.alarmclock.ClockContract$AlarmsColumns !
    
    Change-Id: I34dda84473633a1c56e0c9158baa946ceced15d4

--- src/com/android/phone/MobileNetworkSettings.java
+++ src/com/android/phone/MobileNetworkSettings.java
@@ -70,8 +70,6 @@ import android.widget.TabHost.TabContentFactory;
 import android.widget.TabHost.TabSpec;
 import android.widget.TabHost;
 
-import cyanogenmod.providers.CMSettings;
-
 /**
  * "Mobile network settings" screen.  This preference screen lets you
  * enable/disable mobile data, and control data roaming and other
@@ -883,8 +881,8 @@ public class MobileNetworkSettings extends PreferenceActivity
             root.removePreference(mButtonEnabledNetworks);
         }
 
-        boolean COLPEnabled = CMSettings.System.getInt(getContentResolver(),
-                    CMSettings.System.CONNECTED_LINE_IDENTIFICATION, 1) != 0;
+        boolean COLPEnabled = Settings.Global.getInt(getContentResolver(),
+                    Settings.Global.CONNECTED_LINE_IDENTIFICATION, 1) != 0;
         mButtonCOLP.setChecked(COLPEnabled);
     }
 
@@ -1040,8 +1038,8 @@ public class MobileNetworkSettings extends PreferenceActivity
                     android.provider.Settings.System.MVNO_ROAMING, value ? 1 : 0);
             return true;
         } else if (preference == mButtonCOLP) {
-            CMSettings.System.putInt(getContentResolver(),
-                    CMSettings.System.CONNECTED_LINE_IDENTIFICATION,
+            Settings.Global.putInt(getContentResolver(),
+                    Settings.Global.CONNECTED_LINE_IDENTIFICATION,
                     mButtonCOLP.isChecked() ? 0 : 1);
         }
 
